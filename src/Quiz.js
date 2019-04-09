a4841c726d818690d5d453feb57ebe2c2f20d3a7
ivanlopezmolina <ivanlopezmolina@gmail.com>
2019-04-08 22:46:01 -0400

    Update Quiz.js
    Displaying Quiz Questions and Handling Events

--- src/Quiz.js
+++ src/Quiz.js
@@ -20,8 +20,8 @@ class Quiz extends Component {
         return(            
         <div>
             {isQuizEnd ? <QuizEnd /> : 
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
-                showNextQuestionHandler={this.showNextQuestion.bind(this)}/>
+                <QuizQuestion quiz_question={quizData.quiz_questions
+                    [this.state.quiz_position - 1]}showNextQuestionHandler={this.showNextQuestion.bind(this)}/>
             }                
         </div>
         )
